275807628725c8bcb3d4d2b71eae3e3b9b3e6d1e
Shelamov Ruslan <shelamov.ruslan@gmail.com>
2022-06-15 15:25:44 +0300

    3

--- src/main/java/PolishNotation.java
+++ src/main/java/PolishNotation.java
@@ -13,45 +13,69 @@ public class PolishNotation {
     public PolishNotation(ArrayList<String> elementsExpression) {
         this.inverseNotation = new ArrayList<>();
         this.stack = new Stack();
+
         for (String element : elementsExpression) {
-            if (element.equals("(")) {
-                stack.push(element);
-            } else if (element.equals(")")) {
-                while (!stack.peek().equals("(")) {
-                    inverseNotation.add(stack.pop());
-                }
-                if (stack.peek().equals("(")) {
-                    stack.pop();
-                }
-            } else if (element.equals("+") || element.equals("-")) {
-                if (!stack.isEmpty()) {
-                    while (stack.peek().equals("*") || stack.peek().equals("/") || stack.peek().equals("+") || stack.peek().equals("-")) {
-                        inverseNotation.add(stack.pop());
-                        if (stack.isEmpty()){
-                            break;
-                        }
-                    }
-                }
-                stack.push(element);
-            } else if (element.equals("*") || element.equals("/")) {
-                if (stack.peek().equals("+") || stack.peek().equals("-")) {
-                    stack.push(element);
-                } else if (stack.peek().equals("*") || stack.peek().equals("/")) {
-
-                        while (stack.peek().equals("*") || stack.peek().equals("/")) {
-                            inverseNotation.add(stack.pop());
-                        }
-
-                    stack.push(element);
-                } else if (stack.peek().equals("(")) {
-                    stack.push(element);
-                }
-            } else {
-                inverseNotation.add(element);
+            switch (element) {
+                case "(" -> bracketOpen(element);
+                case ")" ->  bracketClose(element);
+                case "+", "-" ->  plus (element);
+                case "*", "/" -> multiplication(element);
+                default -> inverseNotation.add(element);
             }
         }
+
         while (!stack.isEmpty()){
             inverseNotation.add(stack.pop());
         }
    }
+
+   public void bracketOpen (String element) {
+       if (element.equals("(")) {
+           stack.push(element);
+       }
+   }
+    public void bracketClose (String element) {
+        if (element.equals(")")) {
+            while (!stack.peek().equals("(")) {
+                inverseNotation.add(stack.pop());
+            }
+            if (stack.peek().equals("(")) {
+                stack.pop();
+            }
+        }
+    }
+    public void plus (String element) {
+        if (element.equals("+") || element.equals("-")) {
+            if (!stack.isEmpty()) {
+                while (stack.peek().equals("*") || stack.peek().equals("/") || stack.peek().equals("+") || stack.peek().equals("-")) {
+                    inverseNotation.add(stack.pop());
+                    if (stack.isEmpty()){
+                        break;
+                    }
+                }
+            }
+            stack.push(element);
+        }
+    }
+    public void multiplication (String element) {
+        if (element.equals("*") || element.equals("/")) {
+            if (stack.peek().equals("+") || stack.peek().equals("-")) {
+                stack.push(element);
+            } else if (stack.peek().equals("*") || stack.peek().equals("/")) {
+
+                while (stack.peek().equals("*") || stack.peek().equals("/")) {
+                    inverseNotation.add(stack.pop());
+                }
+
+                stack.push(element);
+            } else if (stack.peek().equals("(")) {
+                stack.push(element);
+            }
+        }
+    }
+
+
+
+
 }
+
